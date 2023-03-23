8a0af3269fdafcdf7f835fbacf1b5ae1599ab35d
TimSchneider1 <timleander03@gmail.com>
2023-03-23 16:05:42 +0100

    test

--- A_01/structure.js
+++ A_01/structure.js
@@ -1,5 +1,7 @@
 var A01;
 (function (A01) {
-    let sentenceOne = [];
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
 })(A01 || (A01 = {}));
 //# sourceMappingURL=structure.js.map
--- A_01/structure.ts
+++ A_01/structure.ts
@@ -1,3 +1,7 @@
 namespace A01{
-    let sentenceOne:string [] = []
+    let subjects:string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicates:string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects:string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
+
+   
 }
