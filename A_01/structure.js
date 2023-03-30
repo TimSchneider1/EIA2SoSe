36b0ef8786ac6fc109f5d47e0d623c2e5bfd2978
TimSchneider1 <timleander03@gmail.com>
2023-03-30 11:50:42 +0200

    test

--- A_01/structure.js
+++ A_01/structure.js
@@ -3,5 +3,22 @@ var A01;
     let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
+    let arrayOne = [];
+    let arrayTwo = [];
+    let arrayThree = [];
+    console.log();
+    for (let i = 6; i <= 0; i--) {
+        let paragraph = getVerse(arrayOne, arrayTwo, arrayThree);
+        console.log(paragraph);
+        console.log();
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        let x = Math.floor(Math.random() * 6);
+        let y = Math.floor(Math.random() * 6);
+        let z = Math.floor(Math.random() * 6);
+        let poem;
+        poem = _subjects.splice(x, 1)[0] + " " + _predicates.splice(y, 1)[0] + " " + _objects.splice(z, 1)[0];
+        return poem;
+    }
 })(A01 || (A01 = {}));
 //# sourceMappingURL=structure.js.map
--- A_01/structure.ts
+++ A_01/structure.ts
@@ -3,5 +3,29 @@ namespace A01{
     let predicates:string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects:string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "Die Karte des Rumtreibers", "Dementoren"];
 
-   
+    let arrayOne:string [] = [];
+    let arrayTwo:string [] = [];
+    let arrayThree:string [] = [];
+
+    console.log()
+
+
+    for (let i = 6; i<=0; i--){
+        let paragraph:string = getVerse(arrayOne, arrayTwo, arrayThree);
+        console.log(paragraph)
+        console.log()
+    } 
+
+    function getVerse(_subjects:string[], _predicates:string[], _objects:string[]):string{
+        let x:number = Math.floor(Math.random() * 6);
+        let y:number = Math.floor(Math.random() * 6);
+        let z:number = Math.floor(Math.random() * 6);
+
+        let poem:string;
+
+        poem = _subjects.splice(x,1)[0]+" "+_predicates.splice(y,1)[0]+" "+_objects.splice(z,1)[0];
+
+        return poem;
+
+    }
 }
