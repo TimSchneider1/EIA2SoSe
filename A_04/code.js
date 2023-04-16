1eba3d989c37ebc89c2c2cff404fb9b6e16a80bd
TimSchneider1 <timleander03@gmail.com>
2023-04-16 14:13:10 +0200

    test

---
+++ A_04/code.js
@@ -0,0 +1,65 @@
+var A_04;
+(function (A_04) {
+    // Tim Schneider (272018)
+    // Kopiert von Benedikt Dettling
+    const any = document.getElementById("newTask");
+    addBtn.addEventListener("click", addTask);
+    function addTask() {
+        const taskInput = document.querySelector("input[name='task']");
+        const commentInput = document.querySelector("input[name='comment']");
+        const personInput = document.querySelector("input[name='person']");
+        const deadlineInput = document.querySelector("input[name='deadline']");
+        const newTask = document.createElement("div");
+        newTask.className = "task";
+        const doneInput = document.createElement("input");
+        doneInput.type = "checkbox";
+        doneInput.name = "done";
+        newTask.appendChild(doneInput);
+        const newTaskInput = document.createElement("input");
+        newTaskInput.type = "text";
+        newTaskInput.name = "task";
+        newTaskInput.placeholder = "Aufgabe";
+        newTaskInput.value = taskInput.value;
+        newTask.appendChild(newTaskInput);
+        const newCommentInput = document.createElement("input");
+        newCommentInput.type = "text";
+        newCommentInput.name = "comment";
+        newCommentInput.placeholder = "Kommentar";
+        newCommentInput.value = commentInput.value;
+        newTask.appendChild(newCommentInput);
+        const newPersonInput = document.createElement("input");
+        newPersonInput.type = "text";
+        newPersonInput.name = "person";
+        newPersonInput.placeholder = "Bearbeiter";
+        newPersonInput.value = personInput.value;
+        newTask.appendChild(newPersonInput);
+        const newDeadlineInput = document.createElement("input");
+        newDeadlineInput.type = "datetime-local";
+        newDeadlineInput.name = "deadline";
+        newDeadlineInput.value = deadlineInput.value;
+        newTask.appendChild(newDeadlineInput);
+        const inProgressLabel = document.createElement("label");
+        inProgressLabel.textContent = "in Bearbeitung:";
+        newTask.appendChild(inProgressLabel);
+        const inProgressInput = document.createElement("input");
+        inProgressInput.type = "checkbox";
+        inProgressInput.name = "inprogress";
+        newTask.appendChild(inProgressInput);
+        const editBtn = document.createElement("button");
+        editBtn.textContent = "Ändern";
+        newTask.appendChild(editBtn);
+        editBtn.addEventListener("click", editTask);
+        const trashIcon = document.createElement("i");
+        trashIcon.className = "fa fa-trash-alt";
+        trashIcon.id = "trashbin";
+        newTask.appendChild(trashIcon);
+        trashIcon.addEventListener("click", deleteTask);
+        const taskList = document.querySelector("#div");
+        taskList.appendChild(newTask);
+        taskInput.value = "";
+        commentInput.value = "";
+        personInput.value = "";
+        deadlineInput.value = "";
+    }
+})(A_04 || (A_04 = {}));
+//# sourceMappingURL=code.js.map
